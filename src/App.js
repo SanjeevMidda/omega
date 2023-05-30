a3a6eabe0af05c7b99dc4a378b3c49438955055b
Sanjeev Midda <110365686+SanjeevMidda@users.noreply.github.com>
2023-05-30 17:01:39 +0100

    Add files via upload
    Uploading source files.

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './index.css';
+import omega from "./imgs/Group 4.svg"
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <h3>WORKS</h3>
+        <h3>EXHIBITION</h3>
+        <h3>ABOUT</h3>
+      </nav>
+      <img src={omega} alt="" />
+
+      <footer>
+        <p>®</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
